d48389fc2e924ff7857559fc358bc59c78bbc36f
Lex <mr.x97@mail.ru>
2019-02-23 20:11:01 +0200

    Showing save dialog

--- javascripts/MenuActionPanel/MenuActionPanel.jsx
+++ javascripts/MenuActionPanel/MenuActionPanel.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./MenuActionPanel.scss";
 import { connect } from "react-redux";
+const { dialog } = require('electron').remote;
 
 class MenuActionPanel extends React.Component {
     render() {
         return (
             <div style={{visibility: this.props.toggleMenuBar.menuBarVisible?'visible':'hidden'}} className='menuActionPanel'>
-                <span className="menuBarActionButton">Save</span>
+                <span className="menuBarActionButton" onClick={() => console.log(dialog.showSaveDialog())}>Save</span>
                 <span className="menuBarActionButton">Open</span>
             </div>
         )
